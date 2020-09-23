e818f58bcd0783eb36d13a5ef9ccb1270145b05b
sformichella <sformichella@gmail.com>
2020-09-23 16:35:44 -0700

    made nametag with input and background colors

---
+++ app.js
@@ -0,0 +1,40 @@
+// Declare button, input space, and name tag name element
+
+const nameTagButton = document.getElementById("name-tag-input-button");
+
+let nameTagInput = document.getElementById("name-tag-input");
+
+let nameTagName = document.getElementById('name-tag-name');
+
+let bodyElement = document.getElementById('body');
+
+// Change the name
+
+nameTagButton.addEventListener('click', () => {
+    let userInput = nameTagInput.value
+    nameTagName.textContent = userInput
+});
+
+// Background Color Stuff
+
+const backgroundButtonBlue = document.getElementById('blue-color-button');
+
+backgroundButtonBlue.addEventListener('click', () => {
+    if (bodyElement.style.backgroundColor === 'blue') {
+        bodyElement.style.backgroundColor = 'lightgray'
+    }
+    else {
+        bodyElement.style.backgroundColor = 'blue'
+    }
+})
+
+const backgroundButtonGreen = document.getElementById('green-color-button');
+
+backgroundButtonGreen.addEventListener('click', () => {
+    if (bodyElement.style.backgroundColor === 'green') {
+        bodyElement.style.backgroundColor = 'lightgray'
+    }
+    else {
+        bodyElement.style.backgroundColor = 'green'
+    }
+})
